56d3ad1d0edcd5c3ba2650cca0b16d23c0b955c1
lbadino <lbadino@iitrbcssrv28.humanoids.iit.it>
2017-07-24 17:50:33 +0200

    removed directories

--- pce_phonerec/getNeighborFrames.m
+++
@@ -1,7 +0,0 @@
-function nframes = getNeighborFrames(frames,ss,se)
-
-nframes{1} = [frames(1,:);frames(1:end-1,:)];
-nframes{1}(ss,:) = frames(ss,:);
-
-nframes{2} = [frames(2:end,:);frames(end,:)];
-nframes{2}(se,:) = frames(se,:);
--- phonerec/demo/demo.m
+++
@@ -1,8 +0,0 @@
-% demo script for phoneme classification using acoustic-articultory data
-clear all;
-close all;
-%%First run: baseline only audio, no motor
-plosclassify('audio1_motor0_rec0');
-
-%%Second run: raw motor reconstructed from raw audio
-plosclassify('audio1_motor3_rec1');
