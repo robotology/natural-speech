806a388dd430ce0c11130e43c9ecaed089ad6b06
bhigy <bertrand.higy@gmail.com>
2017-08-08 18:00:57 +0200

    [icubrec] prep_cmu_dict.sh - use HTK_DATA/cmu instead of a parameter

--- icubrec/prep_cmu_dict.sh
+++ icubrec/prep_cmu_dict.sh
@@ -7,7 +7,7 @@
 #
 
 USAGE="Prepare CMU dictionary\n"
-USAGE=$USAGE"Usage: $0 [-e envt_file] dict_folder";
+USAGE=$USAGE"Usage: $0 [-e envt_file]";
 
 # ":" for options that require a string argument
 # "#" for options that require a int argument
@@ -27,14 +27,6 @@ done
 # shifting the options index to the next parameter we didn't take care of
 shift $((OPTIND - 1));
 
-DICT_FOLDER=${1%%/}; export DICT_FOLDER
-
-# Check mandatory arguments
-if test -z $DICT_FOLDER; then
-    echo -e $USAGE >&2;
-    exit 1;
-fi
-
 # Source envt file
 if ! test -z $ENVT_FILE; then
     source $ENVT_FILE
@@ -42,22 +34,21 @@ fi
 
 echo "Environment variables:"
 echo "ENVT_FILE       = $ENVT_FILE"
-echo "DICT_FOLDER     = $DICT_FOLDER"
 echo ""
 
-perl $HTK_SCRIPTS/FixCMUDict.pl $DICT_FOLDER/c0.6 >$DICT_FOLDER/cmu6
+perl $HTK_SCRIPTS/FixCMUDict.pl $HTK_DATA/cmu/c0.6 >$HTK_DATA/cmu/cmu6
 
-#perl MergeDict.pl $DICT_FOLDER/cmu6temp $HTK_COMMON/wsj1_extra_dict >$DICT_FOLDER/cmu6
+#perl MergeDict.pl $HTK_DATA/cmu/cmu6temp $HTK_COMMON/wsj1_extra_dict >$HTK_DATA/cmu/cmu6
 
 # Create a dictionary with a sp short pause after each word, this is
 # so when we do the phone alignment from the word level MLF, we get
 # the sp phone inbetween the words.  This version duplicates each
 # entry and uses a long pause sil after each word as well.  By doing
 # this we get about a 0.5% abs increase on Nov92 test set.
-perl $HTK_SCRIPTS/AddSp.pl $DICT_FOLDER/cmu6 1 >$DICT_FOLDER/cmu6sp
+perl $HTK_SCRIPTS/AddSp.pl $HTK_DATA/cmu/cmu6 1 >$HTK_DATA/cmu/cmu6sp
 
 # We need a dictionary that has the word "silence" with the mapping to the sil phone
-cat $DICT_FOLDER/cmu6sp >$DICT_FOLDER/cmu6temp
-echo "silence sil" >>$DICT_FOLDER/cmu6temp
-sort $DICT_FOLDER/cmu6temp >$DICT_FOLDER/cmu6spsil
-rm -f $DICT_FOLDER/cmu6temp
+cat $HTK_DATA/cmu/cmu6sp >$HTK_DATA/cmu/cmu6temp
+echo "silence sil" >>$HTK_DATA/cmu/cmu6temp
+sort $HTK_DATA/cmu/cmu6temp >$HTK_DATA/cmu/cmu6spsil
+rm -f $HTK_DATA/cmu/cmu6temp
