37a2520bee6c3b4b510414071df9cb14584015ae
bhigy <bertrand.higy@gmail.com>
2017-08-21 17:27:26 +0200

    added dnn main training script

---
+++ icubrec/dnn_training/htk/train.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+DESCRIPTION="Train RELU DNN"
+USAGE="Usage: $(basename $0) [-h] [-e envt_file] output_folder
+
+Positional arguments:
+    output_folder   folder where the model is stored
+
+Optional arguments:
+    -e              environment file
+    -h              help"
+
+# ":" for options that require a string argument
+# "#" for options that require a int argument
+while getopts "e:h" opt; do
+    case $opt in
+    e)
+        ENVT_FILE=$OPTARG;;
+    h)
+        echo -e "$DESCRIPTION\n";
+        echo -e "$USAGE";
+        exit 0;;
+    \?)
+        echo -e "$USAGE" >&2;
+        exit 1;;
+    esac
+done
+
+# shifting the options index to the next parameter we didn't take care of
+shift $((OPTIND - 1));
+
+OUTPUT_FOLDER=${1%%/}; export OUTPUT_FOLDER
+
+# Check mandatory arguments
+if test -z $OUTPUT_FOLDER; then
+    echo -e "$USAGE" >&2;
+    exit 1;
+fi
+
+# Source envt file
+if ! test -z $ENVT_FILE; then
+    source $ENVT_FILE
+fi
+
+echo "Environment variables:"
+echo "ENVT_FILE         = $ENVT_FILE"
+echo "OUTPUT_FOLDER     = $OUTPUT_FOLDER"
+echo ""
+
+cd $OUTPUT_FOLDER
+
+### Writing config files ####################
+eval "echo \"$(sed "s:\":\\\\\":g" $HTK_COMMON/HTE.dnn.am)\"" >HTE.dnn.am
+eval "echo \"$(sed "s:\":\\\\\":g" $HTK_COMMON/cvn.cfg)\"" >cvn.cfg
+eval "echo \"$(sed "s:\":\\\\\":g" $HTK_COMMON/connect.hed)\"" >connect.hed
+eval "echo \"$(sed "s:\":\\\\\":g" $HTK_COMMON/basic.htkc)\"" >basic.htkc
+eval "echo \"$(sed "s:\":\\\\\":g" $HTK_COMMON/train.htkc)\"" >train.htkc
+# ident_cvn
+echo "<VARSCALE> $FEATURE_SIZE" >ident_cvn
+eval printf '"1.0 "%.0s' {1..$FEATURE_SIZE} >>ident_cvn
+# Transcriptions
+merge_mlf.sh $TRANSCR_FILES | sed "s:*:$CORPORA_OTHER/feat/$FEATURE_FOLDER:g" >alignment.mlf
+
+### Preparing prototype ####################
+if [ ! -d "proto" ]; then
+    mkdir -p proto
+fi
+$REPO/icubrec/dnn_training/htk/GenInitDNN.py HTE.dnn.am proto/dnn
+if [ ! -d "dnn6/init" ]; then
+    mkdir -p dnn6/init
+fi
+cp -f $GMM_MODEL_DIR/$GMM_HMMLIST .
+touch proto/foolist
+HHEd -H $GMM_MODEL_DIR/$GMM_HMMDEFS_DIR/$GMM_HMMDEFS -M dnn6/init connect.hed $GMM_HMMLIST
+if [ ! -d "cvn" ]; then
+    mkdir -p cvn
+fi
+sed "s:^:$CORPORA_OTHER/feat/$FEATURE_FOLDER/:g" <$DNN_TR_LIST >./train.scp
+sed "s:^:$CORPORA_OTHER/feat/$FEATURE_FOLDER/:g" <$DNN_DT_LIST >./hv.scp
+HCompV -p *%%%% -k *.%%%% -C cvn.cfg -q mv -c cvn -S train.scp
+
+### Training  ####################
+if [ ! -d "dnn6.trained" ]; then
+    mkdir -p dnn6.trained
+fi
+
+date
+HNTrainSGD -C basic.htkc -C train.htkc -H dnn6/init/$GMM_HMMDEFS -M dnn6.trained -S train.scp -N hv.scp -l LABEL -I alignment.mlf $GMM_HMMLIST
+date
